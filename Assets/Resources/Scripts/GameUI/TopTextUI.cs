26c03daeb26b7643bcbf06db0d21fc929b0466e8
DaKo51 <50943119+DaKo51@users.noreply.github.com>
2019-12-08 10:26:24 +0100

    Update TopTextUI.cs

--- Assets/Resources/Scripts/GameUI/TopTextUI.cs
+++ Assets/Resources/Scripts/GameUI/TopTextUI.cs
@@ -27,7 +27,6 @@ public class TopTextUI : MonoBehaviour
         text = GetComponent<TextMeshProUGUI>();
         text.text = TextFormat(textUI);
         UIValue = carriedValue;
-        Debug.Log(carriedValue);
 
         //Components for Top Text UI
         colorchange = GetComponent<ColorChange>();
